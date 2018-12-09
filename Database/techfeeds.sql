77cbc175a02ac7b16053e4869bfcc32b4219500f
MMRevanth <mmrevanth00@gmail.com>
2018-12-09 18:08:32 +0530

    DB_file

---
+++ Database/techfeeds.sql
@@ -0,0 +1,28 @@
+create table techfeeds (feed_no int , feed_links char(120));
+
+insert into techfeeds values (1,"https://www.fedscoop.com/watson-cloud/");
+
+insert into techfeeds values (2,"https://www.forbes.com/sites/roysmythe/2014/09/22/delivering-health-care-by-drone/#4036112b4917");
+
+insert into techfeeds values (3,"https://www.smartdatacollective.com/demise-data-scientist-heresy-or-fact/");
+
+insert into techfeeds values (4,"https://www.newscientist.com/article/2187599-deepminds-go-playing-software-can-now-beat-you-at-two-more-games/");
+
+insert into techfeeds values (5,"https://www.newscientist.com/article/2186512-exclusive-uk-police-wants-ai-to-stop-violent-crime-before-it-happens/");
+
+insert into techfeeds values (6,"https://www.analyticsindiamag.com/how-qualcomm-investment-in-5-indian-deep-tech-startups-is-paying-off/");
+
+insert into techfeeds values (7,"https://www.analyticsindiamag.com/emergence-of-citizen-data-scientists/");
+
+insert into techfeeds values (8,"https://www.analyticsindiamag.com/5-paradoxes-which-left-ai-researchers-in-a-lurch/");
+
+insert into techfeeds values (9,"https://www.analyticsindiamag.com/5-programming-languages-in-fintech-thatll-earn-you-the-fattest-paycheck/");
+
+insert into techfeeds values (10,"https://www.analyticsindiamag.com/samsung-fakes-portrait-mode-uses-dslr-clicked-photo-as-camera-sample/");
+
+select * from techfeeds
+
+
+
+
+
