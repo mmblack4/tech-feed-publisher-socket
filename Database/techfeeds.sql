7b6301f54267fa1be270c9ec87932abc0f90c932
MMRevanth <mmrevanth00@gmail.com>
2018-12-17 23:59:29 +0530

    New database

--- Database/techfeeds.sql
+++ Database/techfeeds.sql
@@ -1,26 +1,27 @@
-create table techfeeds (feed_no int , feed_links char(120));
+create table techfeeds (feed_no int , feed_links char(120), tags char(50),summary char (120));
 
-insert into techfeeds values (1,"https://www.fedscoop.com/watson-cloud/");
+insert into techfeeds values (1,"https://www.fedscoop.com/watson-cloud/","IBM Watson","The cognitive cloud? IBM rolls out Watson-as-a-service");
 
-insert into techfeeds values (2,"https://www.forbes.com/sites/roysmythe/2014/09/22/delivering-health-care-by-drone/#4036112b4917");
+insert into techfeeds values (2,"https://www.forbes.com/sites/roysmythe/2014/09/22/delivering-health-care-by-drone/#4036112b4917","Technology served Mankind","Delivering Health Care By Drone");
 
-insert into techfeeds values (3,"https://www.smartdatacollective.com/demise-data-scientist-heresy-or-fact/");
+insert into techfeeds values (3,"https://www.smartdatacollective.com/demise-data-scientist-heresy-or-fact/","Data Science","The Demise of the Data Scientist: Heresy or Fact?");
 
-insert into techfeeds values (4,"https://www.newscientist.com/article/2187599-deepminds-go-playing-software-can-now-beat-you-at-two-more-games/");
+insert into techfeeds values (4,"https://www.newscientist.com/article/2187599-deepminds-go-playing-software-can-now-beat-you-at-two-more-games/","Deep Learning","DeepMind’s Go playing software can now beat you at two more games");
 
-insert into techfeeds values (5,"https://www.newscientist.com/article/2186512-exclusive-uk-police-wants-ai-to-stop-violent-crime-before-it-happens/");
+insert into techfeeds values (5,"https://www.newscientist.com/article/2186512-exclusive-uk-police-wants-ai-to-stop-violent-crime-before-it-happens/","Artificial Intelligence","UK police wants AI to stop violent crime before it happens");
 
-insert into techfeeds values (6,"https://www.analyticsindiamag.com/how-qualcomm-investment-in-5-indian-deep-tech-startups-is-paying-off/");
+insert into techfeeds values (6,"https://www.analyticsindiamag.com/how-qualcomm-investment-in-5-indian-deep-tech-startups-is-paying-off/","Tech World Updates","How Qualcomm’s Investment In 5 Indian Deep Tech Startups Is Paying Off");
 
-insert into techfeeds values (7,"https://www.analyticsindiamag.com/emergence-of-citizen-data-scientists/");
+insert into techfeeds values (7,"https://www.analyticsindiamag.com/emergence-of-citizen-data-scientists/","Data Science","Emergence of Citizen Data Scientists");
 
-insert into techfeeds values (8,"https://www.analyticsindiamag.com/5-paradoxes-which-left-ai-researchers-in-a-lurch/");
+insert into techfeeds values (8,"https://www.analyticsindiamag.com/5-paradoxes-which-left-ai-researchers-in-a-lurch/","Artificial Intelligence","5 paradoxes which left AI researchers in a lurch");
 
-insert into techfeeds values (9,"https://www.analyticsindiamag.com/5-programming-languages-in-fintech-thatll-earn-you-the-fattest-paycheck/");
+insert into techfeeds values (9,"https://www.analyticsindiamag.com/5-programming-languages-in-fintech-thatll-earn-you-the-fattest-paycheck/","Money for Knowledge","5 Programming Languages In Fintech That’ll Earn You The Fattest Paycheck");
 
-insert into techfeeds values (10,"https://www.analyticsindiamag.com/samsung-fakes-portrait-mode-uses-dslr-clicked-photo-as-camera-sample/");
+insert into techfeeds values (10,"https://www.analyticsindiamag.com/samsung-fakes-portrait-mode-uses-dslr-clicked-photo-as-camera-sample/","Tech World Updates","Samsung Fakes The Portrait Mode; Uses DSLR-Clicked Photo As Its Camera Sample");
 
-select * from techfeeds
+select * from techfeeds;
+
 
 
 
