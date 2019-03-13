10e98ddc9ef4a6358e7a5222a9ae07406bf7722e
MMRevanth <mmrevanth00@gmail.com>
2019-03-13 22:06:33 +0530

    Initial data

---
+++ datas.py
@@ -0,0 +1,10 @@
+import requests
+import json
+query = 'java'
+api_url = 'https://api.duckduckgo.com/?q='+query+'&format=json&pretty=1'
+response = requests.get(api_url).json()
+print(response)
+results = response['RelatedTopics']['Result']
+if results:
+    first_result_url = results[0]['url']
+    print(first_result_url)
